667594d8b97d6635d049b19c88f4628358925748
Dita indriawati <80086227+dtindriaa@users.noreply.github.com>
2021-03-14 00:31:09 +0700

    Add files via upload

---
+++ Data_Buku.java
@@ -0,0 +1,26 @@
+package PROGRAM.DITA;
+
+import java.util.Scanner;
+
+public class Data_Buku {
+    public static void main(String[] args) {
+
+        String NamaBuku;
+        String KodeBuku;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("==Daftar Buku==");
+        System.out.println("Nama Buku : ");
+        
+        NamaBuku = input.next();
+        
+        System.out.println("Kode Buku : ");
+        
+        
+        KodeBuku = input.next();
+        
+        System.out.println("================");
+        System.out.println("Nama Buku : " + NamaBuku);
+        System.out.println("Kode Buku : " + KodeBuku);
+    }
+}
---
+++ Data_Transaksi.java
@@ -0,0 +1,26 @@
+package PROGRAM.DITA;
+
+import java.util.Scanner;
+
+public class Data_Transaksi {
+     public static void main(String[] args) {
+
+        String NamaBarang;
+        String JumlahTransaksi;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("==Daftar Barang==");
+        System.out.println("Nama Barang : ");
+        
+        NamaBarang = input.next();
+        
+        System.out.println("Jumlah Transaksi : ");
+        
+        
+        JumlahTransaksi = input.next();
+        
+        System.out.println("================");
+        System.out.println("Nama Barang : " + NamaBarang);
+        System.out.println("Jumlah Transaksi : " + JumlahTransaksi);
+    }
+}
